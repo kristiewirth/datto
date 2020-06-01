ef6166044f734fa4b7a8279244257510a184b4c8
kristiewirth <kristie.ann.wirth@gmail.com>
2020-06-01 11:03:23 -0600

    Create package

--- tests/test_clean_text.py
+++ tests/test_clean_text.py
@@ -5,11 +5,11 @@ ct = CleanText()
 
 def test_remove_names():
     text = "Hello John, how are you doing?"
-    cleaned_text = ct.removePiiNames(text)
+    cleaned_text = ct.remove_names(text)
     assert "John" not in cleaned_text
 
 
 def test_remove_links():
     text = "Here's a link: www.google.com Thanks!"
-    cleaned_text = ct.removeLinks(text)
+    cleaned_text = ct.remove_links(text)
     assert "www.google.com" not in cleaned_text
