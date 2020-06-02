c0aad854afd29901ba66d03925f24b75d285789a
kristiewirth <kristie.ann.wirth@gmail.com>
2020-06-02 14:25:37 -0600

    Add init imports

--- datto/__init__.py
+++ datto/__init__.py
@@ -1,2 +1,5 @@
 from datto.CleanText import CleanText
 from datto.Experiments import Experiments
+from datto.DataConnections import DataConnections
+from datto.DataConnections import KafkaInterface
+from datto.Setup import Setup
