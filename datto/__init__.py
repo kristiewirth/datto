bc2f5beec6765b6f9e517b12bce22562e485ffd8
kristiewirth <kristie.ann.wirth@gmail.com>
2020-06-23 12:42:53 -0600

    Add train and results to init

--- datto/__init__.py
+++ datto/__init__.py
@@ -3,3 +3,5 @@ from datto.Experiments import Experiments
 from datto.DataConnections import DataConnections
 from datto.DataConnections import KafkaInterface
 from datto.Setup import Setup
+from datto.TrainModel import TrainModel
+from datto.ModelResults import ModelResults
