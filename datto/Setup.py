69756b31e2c30f13eb424d5afcda1d10e88ac5f2
kristiewirth <kristie.ann.wirth@gmail.com>
2020-06-23 12:42:09 -0600

    Add doc string

--- datto/Setup.py
+++ datto/Setup.py
@@ -1,10 +1,16 @@
 import logging
-from pythonjsonlogger import jsonlogger
 import sys
+
+from pythonjsonlogger import jsonlogger
 
 
 class Setup:
     def setup_logger(self):
+        """
+        Returns
+        --------
+        logger: jsonlogger
+        """
         logger = logging.getLogger()
         logging.basicConfig(
             format="%(levelname) -10s %(asctime)s %(module)s at line %(lineno)d: %(message)s",
