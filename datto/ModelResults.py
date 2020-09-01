99375902ce93be0e14f19eaf0b0832e542b61183
kristiewirth <kristie.ann.wirth@gmail.com>
2020-09-01 13:45:21 -0600

    Lower max topics

--- datto/ModelResults.py
+++ datto/ModelResults.py
@@ -74,7 +74,7 @@ class ModelResults:
             # In case 1, add 1 to get at least 2
             # The rest are based on eyeballing numbers
             min_topics = ceil(X.shape[0] * 0.01) + 1
-            max_topics = ceil(X.shape[0] * 0.4)
+            max_topics = ceil(X.shape[0] * 0.2)
             step = ceil((max_topics - min_topics) / 5)
 
             topic_nums = list(np.arange(min_topics, max_topics, step))
