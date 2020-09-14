e9a4ccc05b5bd4f08ed80872162f8192fadf2d2b
kristiewirth <kristie.ann.wirth@gmail.com>
2020-09-14 12:26:19 -0600

    Add hypothesis to front end testing

--- tests/test_front_end.py
+++ tests/test_front_end.py
@@ -1,13 +1,18 @@
 import pandas as pd
+from hypothesis import given
+from hypothesis.extra.pandas import column, data_frames
+
 from datto.FrontEnd import FrontEnd
 
 fe = FrontEnd()
 
-df = pd.DataFrame(["opt1", "opt2", "opt3"], columns=["text"])
 
-
-def test_dropdown_from_dataframe():
+@given(
+    data_frames(
+        columns=[column(name="text", dtype=str), column(dtype=int), column(dtype=bool),]
+    )
+)
+def test_dropdown_from_dataframe(df):
     html_text = fe.dropdown_from_dataframe("Choose an option", df, "text")
 
     assert "<select" in html_text
-
