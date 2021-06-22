0b7dfbfdb0e3fb3418ad9642c86216b9cf1ed202
kristiewirth <kristie.ann.wirth@gmail.com>
2021-06-22 13:43:33 -0600

    Add test for display data

--- tests/test_setup_methods.py
+++ tests/test_setup_methods.py
@@ -8,3 +8,7 @@ s = SetupMethods()
 def test_setup_logger():
     logger = s.setup_logger()
     assert isinstance(logger, logging.RootLogger)
+
+
+def test_display_more_data():
+    s.display_more_data(10)
