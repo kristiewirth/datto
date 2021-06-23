0a4dfffcd47171fe7a91cdb4198597e4d322da36
kristiewirth <kristie.ann.wirth@gmail.com>
2021-06-23 12:38:47 -0600

    Updating name of cleaning class

---
+++ datto/CleanDataframe.py
@@ -0,0 +1,335 @@
+import os
+import re
+import string
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+import spacy
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en")
+except Exception:
+    download("en")
+    nlp = spacy.load("en")
+
+
+class CleanDataframe:
+    def remove_names(self, text):
+        """
+        Parameters
+        --------
+        text: str
+
+        Returns
+        --------
+        cleaned_text: str
+
+        """
+        all_names = pd.read_pickle(
+            os.path.join(os.path.dirname(__file__), "data/all_names")
+        )
+        cleaned_text = text
+        for _, row in all_names.iterrows():
+            # Matches name as long as it is not followed by lowercase characters
+            # Removing names that are a part of another word
+            cleaned_text = re.sub(row["name"] + "(?![a-z])", " ", cleaned_text)
+        return cleaned_text
+
+    def remove_pii(self, text):
+        """
+        Remove common patterns of personally identifiable information (PII)
+        Parameters
+        --------
+        text: str
+        Returns
+        --------
+        cleaned_text: str
+        """
+
+        regex_dict = {
+            "credit_card_numbers": r"(?:\d[ -]*?){13,16}",
+            "phone_numbers": r"[\+]?[\d]{0,3}[\s]?[\(]?\d{3}[\)]?[\s\-\.]{0,1}\d{3}[\s\-\.]{0,1}\d{4}",
+            "social_security_numbers": r"(\d{3}[-\s]?\d{2}[-\s]?\d{4})",
+            "ip_addresses": r"((?:[0-9]{1,3}\.){3}[0-9]{1,3})",
+            "email_addresses": r"([a-zA-Z0-9_\.-]+)@([1-9a-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})",
+            "urls": r"((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,255}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))",
+        }
+
+        cleaned_text = text
+        for this_pii_item in regex_dict:
+            cleaned_text = re.sub(regex_dict[this_pii_item], "", cleaned_text,)
+
+        return cleaned_text
+
+    def remove_links(self, text):
+        """
+        Parameters
+        --------
+        text: str
+
+        Returns
+        --------
+        cleaned_text: str
+
+        """
+        cleaned_text = text
+        links_found = re.findall(
+            r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})",
+            cleaned_text,
+        )
+        for link in links_found:
+            cleaned_text = cleaned_text.replace(link, "")
+        return cleaned_text
+
+    def lematize(self, text):
+        """
+        Parameters
+        --------
+        text: str
+
+        Returns
+        --------
+        list of spacy tokens
+
+        """
+        spacy_text = nlp(text)
+        return [token.lemma_ for token in spacy_text if not token.is_space]
+
+    def remove_email_greetings_signatures(self, text):
+        """
+        In order to obtain the main text of an email only, this method removes greetings, signoffs,
+        and signatures by identifying sentences with less than 5% verbs to drop. Does not replace links.
+
+        Inspiration from: https://github.com/mynameisvinn/EmailParser
+
+        Parameters
+        --------
+        text: str
+
+        Returns
+        --------
+        text: str
+
+        """
+        sentences = text.strip().split("\n")
+        non_sentences = []
+
+        for sentence in sentences:
+            spacy_text = nlp(sentence.strip())
+            verb_count = np.sum(
+                [
+                    (
+                        token.pos_ == "VERB"
+                        or token.pos_ == "AUX"
+                        or token.pos_ == "ROOT"
+                        or token.pos_ == "pcomp"
+                    )
+                    for token in spacy_text
+                ]
+            )
+            try:
+                prob = float(verb_count) / len(spacy_text)
+            except Exception:
+                prob = 1.0
+
+            # If 5% or less of a sentence is verbs, it's probably not a real sentence
+            if prob <= 0.05:
+                non_sentences.append(sentence)
+
+        for non_sentence in non_sentences:
+            # Don't replace links
+            if "http" not in non_sentence and non_sentence not in string.punctuation:
+                text = text.replace(non_sentence, "")
+
+        return text
+
+    def clean_column_names(self, df):
+        """
+        Rename all columns to use underscores to reference columns without bracket formatting
+
+        Parameters
+        --------
+        df: DataFrame
+
+        Returns
+        --------
+        df: DataFrame
+
+        """
+        df.rename(
+            columns=lambda x: str(x).strip().replace(" ", "_").lower(), inplace=True
+        )
+        return df
+
+    def remove_duplicate_columns(self, df):
+        """
+        Remove columns with the same name
+
+        Parameters
+        --------
+        df: DataFrame
+
+        Returns
+        --------
+        df: DataFrame
+
+        """
+        df = df.loc[:, ~df.columns.duplicated()]
+        return df
+
+    def fix_col_data_type(self, df, col, desired_dt):
+        """
+        Change column datatype using the best method for each type.
+
+        Parameters
+        --------
+        df: DataFrame
+        col: str
+            Column to change the dtype for
+        desired_dt: str
+            {'float', 'int', 'datetime', 'str'}
+
+        Returns
+        --------
+        df: DataFrame
+
+        """
+        if desired_dt in ("float", "int"):
+            df[col] = pd.to_numeric(df[col], errors="coerce")
+        elif desired_dt == "datetime":
+            df[col] = pd.to_datetime(df[col], errors="coerce")
+        elif desired_dt == "str":
+            df[col] = df[col].astype(str)
+
+        return df
+
+    def compress_df(self, df):
+        """
+        Compresses each dataframe column as much as possible depending on type and values.
+
+        Parameters
+        --------
+        df: DataFrame
+
+        Returns
+        --------
+        df: DataFrame
+
+        """
+        for col in df.columns:
+            if df[col].dtype == "O":
+                unique_vals = df[col].nunique()
+                count_vals = df[col].shape[0]
+                if unique_vals < (count_vals * 0.5):
+                    df[col] = df[col].astype("category")
+            elif df[col].dtype == "int64":
+                df[col] = pd.to_numeric(df[col], downcast="unsigned")
+            elif df[col].dtype == "float64":
+                df[col] = pd.to_numeric(df[col], downcast="float")
+
+        return df
+
+    def make_uuid(self, id_num):
+        """
+        Make a UUid_num from a text string
+
+        Parameters
+        --------
+        id_num: str
+
+        Returns
+        --------
+        uuid: str
+
+        """
+        return (
+            id_num[:8]
+            + "-"
+            + id_num[8:12]
+            + "-"
+            + id_num[12:16]
+            + "-"
+            + id_num[16:20]
+            + "-"
+            + id_num[20:]
+        )
+
+    def df_most_common_only(self, df, col, num):
+        """
+        From a given column in a dataframe, find the most common values and return a dataframe that has those most common values only.
+
+        Parameters
+        --------
+        df: DataFrame
+        col: str
+        num: int
+
+        Returns
+        --------
+        cleaned_df: DataFrame
+
+        """
+        most_common_lst = [x[0] for x in Counter(df[col]).most_common(num)]
+        df = df[df[col].isin(most_common_lst)]
+        return df
+
+    def batch_pandas_operation(self, df, num_splits, identifier_col, func):
+        """
+        Use a function on a Pandas DataFrame in chunks for faster processing.
+
+        Parameters
+        --------
+        df: DataFrame
+        num_splits: int
+        identifier_col: str
+        func: Function
+
+        Returns
+        --------
+        new_df: DataFrame
+
+        """
+        new_df = pd.DataFrame()
+        ids = df[identifier_col].unique()
+        division = len(ids) / float(num_splits)
+        part_ids = [
+            ids[int(round(division * i)) : int(round(division * (i + 1)))]
+            for i in range(num_splits)
+        ]
+        for lst in part_ids:
+            temp_df = df[df[identifier_col].isin(lst)]
+            new_df = new_df.append(func(temp_df))
+
+        return new_df
+
+    def batch_merge_operation(self, df_1, df_2, num_splits, identifier_col, merge_col):
+        """
+        Merge two Pandas DataFrame in chunks for faster processing.
+
+        Parameters
+        --------
+        df_1: DataFrame
+        df_2: DataFrame
+        num_splits: int
+        identifier_col: str
+        merge_col: str
+
+        Returns
+        --------
+        new_df: DataFrame
+
+        """
+        new_df = pd.DataFrame()
+        ids = df_1[identifier_col].unique()
+        division = len(ids) / float(num_splits)
+        part_ids = [
+            ids[int(round(division * i)) : int(round(division * (i + 1)))]
+            for i in range(num_splits)
+        ]
+        for lst in part_ids:
+            temp_df = df_1[df_1[identifier_col].isin(lst)]
+            temp_df = pd.merge(temp_df, df_2, how="left", on=merge_col)
+            new_df = new_df.append(temp_df)
+        return new_df
--- tests/test_clean_dataframe.py
+++ tests/test_clean_dataframe.py
@@ -2,10 +2,10 @@ import pandas as pd
 from hypothesis import example, given, strategies
 from hypothesis.extra.pandas import column, data_frames
 
-from datto.CleanText import CleanText
+from datto.CleanDataframe import CleanDataframe
 
 
-ct = CleanText()
+cdf = CleanDataframe()
 
 df = pd.DataFrame(
     [
@@ -33,21 +33,21 @@ def replace_text_test(df):
 
 def test_remove_names():
     text = "Hello John, how are you doing?"
-    cleaned_text = ct.remove_names(text)
+    cleaned_text = cdf.remove_names(text)
     assert "John" not in cleaned_text
 
 
 @given(strategies.text())
 @example("Here's a link: www.google.com Thanks!")
 def test_remove_links(text):
-    cleaned_text = ct.remove_links(text)
+    cleaned_text = cdf.remove_links(text)
     assert ".com" not in cleaned_text
 
 
 @given(strategies.text())
 @example("I went running today.")
 def test_lematize(text):
-    spacy_tokens = ct.lematize(text)
+    spacy_tokens = cdf.lematize(text)
     assert "ing " not in spacy_tokens
 
 
@@ -65,14 +65,14 @@ def test_lematize(text):
     """
 )
 def test_remove_email_greetings_signatures(text):
-    cleaned_body = ct.remove_email_greetings_signatures(text)
+    cleaned_body = cdf.remove_email_greetings_signatures(text)
     assert "Hello" not in cleaned_body
 
 
 @given(data_frames(columns=[column(dtype=str), column(dtype=int), column(dtype=bool),]))
 @example(pd.DataFrame([["Kristie", "Smith"]], columns=["First Name", "Last Name"]))
 def test_clean_column_names(df):
-    cleaned_df = ct.clean_column_names(df)
+    cleaned_df = cdf.clean_column_names(df)
     assert " " not in cleaned_df.columns
 
 
@@ -84,7 +84,7 @@ def test_clean_column_names(df):
     )
 )
 def test_remove_duplicate_columns(df):
-    cleaned_df = ct.remove_duplicate_columns(df)
+    cleaned_df = cdf.remove_duplicate_columns(df)
     assert cleaned_df.shape[1] <= df.shape[1]
 
 
@@ -103,9 +103,9 @@ def test_remove_duplicate_columns(df):
     )
 )
 def test_fix_col_data_type(df):
-    df = ct.fix_col_data_type(df, "to_number", "int")
-    df = ct.fix_col_data_type(df, "to_datetime", "datetime")
-    df = ct.fix_col_data_type(df, "to_str", "str")
+    df = cdf.fix_col_data_type(df, "to_number", "int")
+    df = cdf.fix_col_data_type(df, "to_datetime", "datetime")
+    df = cdf.fix_col_data_type(df, "to_str", "str")
 
     assert df.to_number.dtype == "int64" or df.to_number.dtype == "float64"
     assert df.to_datetime.dtype == "<M8[ns]"
@@ -113,15 +113,15 @@ def test_fix_col_data_type(df):
 
 
 def test_compress_df():
-    compressed_df = ct.compress_df(df)
+    compressed_df = cdf.compress_df(df)
     assert compressed_df["int"].dtype == "uint8"
     assert compressed_df["float"].dtype == "float32"
     assert compressed_df["text"].dtype == "category"
 
 
 def test_make_uuid():
-    id = "609390d88cff44269c2e293bd6b89a0b"
-    uuid = ct.make_uuid(id)
+    id_num = "609390d88cff44269c2e293bd6b89a0b"
+    uuid = cdf.make_uuid(id_num)
     assert uuid == "609390d8-8cff-4426-9c2e-293bd6b89a0b"
 
 
@@ -129,14 +129,14 @@ def test_most_common_only():
     col = "text"
     num = 1
 
-    df_cleaned = ct.df_most_common_only(df, col, num)
+    df_cleaned = cdf.df_most_common_only(df, col, num)
     assert df_cleaned.shape[0] < df.shape[0]
 
 
 def test_batch_pandas_operation():
     num_splits = 2
     identifier_col = "int"
-    new_df = ct.batch_pandas_operation(
+    new_df = cdf.batch_pandas_operation(
         df, num_splits, identifier_col, replace_text_test
     )
     assert not new_df["text"].apply(lambda x: "i like" in x).max()
@@ -148,7 +148,7 @@ def test_batch_merge_operation():
     num_splits = 2
     identifier_col = "int"
     merge_col = "int"
-    merged_df = ct.batch_merge_operation(
+    merged_df = cdf.batch_merge_operation(
         df_1, df_2, num_splits, identifier_col, merge_col
     )
     assert merged_df.shape[1] == 5
@@ -165,7 +165,7 @@ def test_remove_pii():
         check out my cool site https://www.stuff.aol.uk.us.biz.us?param=1&other_param=2
         btw, i've submitted like 500 support tickets and i'm furious!
     """
-    cleaned_text = ct.remove_pii(text)
+    cleaned_text = cdf.remove_pii(text)
     assert "cool.guy.123" not in cleaned_text
     assert "123" not in cleaned_text
     assert "https" not in cleaned_text
