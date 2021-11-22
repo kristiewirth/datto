195ef59e5b3aebaafd439841bf41681df098d45a
kristiewirth <kristie.ann.wirth@gmail.com>
2021-11-22 14:07:31 -0700

    Fix import

--- datto/__init__.py
+++ datto/__init__.py
@@ -2,6 +2,7 @@ from datto.CleanDataframe import CleanDataframe
 from datto.Experiments import Experiments
 from datto.DataConnections import SQLConnections
 from datto.DataConnections import S3Connections
+from datto.DataConnections import NotebookConnections
 from datto.SetupMethods import SetupMethods
 from datto.TrainModel import TrainModel
 from datto.ModelResults import ModelResults
